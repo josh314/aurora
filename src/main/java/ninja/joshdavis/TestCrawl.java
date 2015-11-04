e776f65b2f2afdbf08bbf382e4c7f1f5ad5697b5
Joshua Davis <josh314@gmail.com>
2015-11-04 11:49:54 -0500

    sample url list edited

--- src/main/java/ninja/joshdavis/TestCrawl.java
+++ src/main/java/ninja/joshdavis/TestCrawl.java
@@ -5,7 +5,11 @@ public class TestCrawl
     public static void main(String[] args)
     {
         System.out.println("Starting TestCrawl.");
-        String[] initial_queue = {"http://bfttyfgfhjfghjgjkghg.com/"};
+        String[] initial_queue = {
+            "http://www.google.com/",
+            "http://bfttyfgfhjfghjgjkghg.com/"
+        };
+        
         Crawler crawlBaby = new Crawler(initial_queue);
         crawlBaby.crawl();
     }
