aabf047e00b7f9221c0163a81527d475fdf9c0cc
Joshua Davis <josh314@gmail.com>
2015-11-04 16:19:19 -0500

    Crawler digests an initial queue and schedules for asynchronous requesting. Returns after all initial rqeuests have been processed.

--- src/main/java/ninja/joshdavis/Crawler.java
+++ src/main/java/ninja/joshdavis/Crawler.java
@@ -1,29 +1,31 @@
 package ninja.joshdavis;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.concurrent.Future;
 import java.util.concurrent.ExecutionException;
 
 import com.ning.http.client.*;
 
 public class Crawler {
-    private ArrayList<String> queue;
+    private HashSet<String> queue;
     private AsyncHttpClient client;
+    private HashSet<Future<Response>> processing;
 
     public Crawler(String[] _queue) {
-        this.queue = new ArrayList<String>(_queue.length);
+        this.queue = new HashSet<String>(_queue.length);
         for(int i=0; i < _queue.length; ++i) {
             queue.add(_queue[i]);
         }
         this.client = new AsyncHttpClient();
+        this.processing = new HashSet<Future<Response>>(_queue.length);
     }
-    public Crawler(ArrayList<String> _queue) {
-        queue = _queue;//Copy it?
+    public Crawler(HashSet<String> _queue) {
+        queue = new HashSet<String>(_queue);
     }
 
     private void request(String url) {
-        this.client.prepareGet(url).execute(new AsyncCompletionHandler<Response>(){ 
+        Future<Response> f = this.client.prepareGet(url).execute(new AsyncCompletionHandler<Response>(){ 
                 @Override
                 public Response onCompleted(Response response) throws Exception{
                     // Do something with the Response
@@ -38,12 +40,27 @@ public class Crawler {
                     System.out.println(t);
                 }
             });
+        //        this.queue.remove(url);
+        this.processing.add(f);
+    }
+
+    private boolean notDone() {
+        boolean res = true;
+        if(this.queue.isEmpty()) {
+            for(Future<Response> f: this.processing) {
+                res &= f.isDone();
+            }
+        }
+        return res;
     }
     
     public void crawl() {
         for(String url : this.queue) {
             this.request(url);
         }
+        this.queue.clear();
+        while(this.notDone()) {}
+        this.client.close();
     }
 
     public void print()
