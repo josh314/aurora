4f263e702f604c3495de030d006e8a079e7d6689
Joshua Davis <josh314@gmail.com>
2015-11-06 15:56:54 -0500

    Reading intial urls from file in TestCrawl

--- src/main/java/ninja/joshdavis/TestCrawl.java
+++ src/main/java/ninja/joshdavis/TestCrawl.java
@@ -1,29 +1,35 @@
 package ninja.joshdavis;
 
-import java.util.Vector;
-import java.util.Arrays;
+
+import java.util.*;
+import java.io.*;
+import java.nio.file.Paths;
+
+
 
 public class TestCrawl
 {
     public static void main(String[] args)
     {
 
-        String[] initial_queue = {
-            "http://www.google.com/",
-            "http://www.cnn.com/",
-            "http://www.wikipedia.org/wiki/Barack_Obama",
-            "http://www.wikipedia.org/wiki/Mathematics",
-            "http://www.wikipedia.org/wiki/Daredevil",
-            "http://www.wikipedia.org/wiki/Star",
-            "http://www.wikipedia.org/wiki/Java",
-            "http://www.wikipedia.org/",
-            "http://www.arxiv.org",
-            "http://www.amazon.com",
-            "http://www.hockeybuzz.com"
-        };
+        Vector<String> urls = new Vector<String>();
+        // Open file, read line by line and put contents into Vector
+        String filename = "initial_urls.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                urls.add(line);
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File:" + filename + " does not exist.");
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
         
         System.out.println("Starting test crawl.");
-        Vector<String> urls = new Vector<String>(Arrays.asList(initial_queue));
+        //Vector<String> urls = new Vector<String>(Arrays.asList(initial_queue));
         Crawler crawlBaby = new Crawler(urls);
         crawlBaby.crawl();
     }
