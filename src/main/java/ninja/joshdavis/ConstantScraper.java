dfe9611ed44f85850defc11e75b4ebc971fd23f9
Joshua Davis <josh314@gmail.com>
2015-11-10 17:45:54 -0500

    cleanup

--- src/main/java/ninja/joshdavis/ConstantScraper.java
+++ src/main/java/ninja/joshdavis/ConstantScraper.java
@@ -2,10 +2,6 @@ package ninja.joshdavis;
 
 import java.util.*;
 
-import org.jsoup.*;
-import org.jsoup.nodes.*;
-import org.jsoup.select.*;
-
 public class ConstantScraper implements Scraper {
     public List<String> process(String html, String url) {
         return Arrays.asList("http://www.cnn.com","http://www.hockeybuzz.com");
